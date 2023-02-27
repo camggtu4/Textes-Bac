9d01c105d57e34d2b92e0a405fef9faf8d7b9c1f
camggtu4 <91284731+camggtu4@users.noreply.github.com>
2023-02-27 22:33:58 +0100

    Add files via upload

---
+++ Français/textes6-8/js/script.js
@@ -0,0 +1,14 @@
+
+const text1 = document.querySelector('.text1');
+const text2 = document.querySelector('.text2');
+const text3 = document.querySelector('.text3');
+
+function display1() {
+	text1.classList.toggle("hide");
+}
+function display2() {
+	text2.classList.toggle("hide");
+}
+function display3() {
+	text3.classList.toggle("hide");
+}
